3de4a1bd7b868f529b9abb1bd5089b8f48af5371
Thiago-siqueira <thiago_sique@hotmail.com>
2021-06-23 19:20:51 -0300

    quinto exercicio de javascript

---
+++ exercizes/introducao-a-javascript-e-logica-de-programacao_1/triangulo.js
@@ -0,0 +1,12 @@
+let anguloA = 60;
+let anguloB = 60;
+let anguloC = -22;
+
+
+if (anguloA + anguloB + anguloC == 180) {
+    console.log(true);
+} else if (anguloA < 0 || anguloB < 0 || anguloC < 0) {
+    console.log("angulo não é válido!!")
+} else {
+    console.log(false);
+}
