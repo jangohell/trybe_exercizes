a5acbd20f177824fc799b420219b49504c445b7f
Thiago-siqueira <thiago_sique@hotmail.com>
2021-06-23 19:57:31 -0300

    sexto exercicio de javascript

---
+++ exercizes/introducao-a-javascript-e-logica-de-programacao_1/xadrez.js
@@ -0,0 +1,10 @@
+let piece = "ROOK";
+let pieceLower = piece.toLowerCase();
+
+
+
+if (pieceLower == "rook") {
+    console.log("Rook > horizontally or vertically");
+} else {
+    console.log("Invalid Piece");
+}
