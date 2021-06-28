c2bec0f49651e91a2b906765c90a4378977d74a1
Thiago-siqueira <thiago_sique@hotmail.com>
2021-06-28 19:18:56 -0300

    terceiro exercicio da parte 2 do bloco 4 de javascript

---
+++ exercizes/introducao-a-javascript-e-logica-de-programacao_4/parte2_exercicio3.js
@@ -0,0 +1,14 @@
+let numeros = [14, 18, 22, 38, 52, 12, 73, 81, 25];
+
+function numeroMaior() {
+    let maior = 0
+    for (let indice in numeros) {
+        if (numeros[maior] > numeros[indice]) {
+            maior = indice;
+        }   
+    }   
+    return maior;
+}
+
+
+console.log(numeroMaior(numeros));
